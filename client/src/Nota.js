0a25bc7ba8414c89fd0a0639a7f575af8866ad20
gastontresca <82352149+gastontresca@users.noreply.github.com>
2021-11-28 22:55:04 -0300

    Update Nota.js

--- client/src/Nota.js
+++ client/src/Nota.js
@@ -1,10 +1,18 @@
-onst Nota = props => {
-  const { title, text } = props;
+const Nota = props => {
+  const { title, text, id, deleteNote, updateNote } = props;
   return (
-    <div className="nota">
-      <h2>{title}</h2>
-      <p>{text}</p>
+    <div class="note">
+      <div className="nota">
+        <h2 class="card-header">{title}</h2>
+        <p class="p-3">{text}</p>
+        <div class="botones btn-group">
+          <button type="button" onClick={() => deleteNote(id)}>Borrar</button>
+          <button type="button" onClick={() => updateNote(id)}>Editar</button>
+        </div >
+
+      </div >
     </div>
+
   );
 };
 
